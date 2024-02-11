d9847b664b34d17c222c481d15f80524ff368a8f
Catherine Liu <catherine@dhcp-10-250-217-210.harvard.edu>
2024-02-11 13:51:03 -0500

    helena's update

--- test.py
+++ test.py
@@ -3,17 +3,22 @@ import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
 
 class SOM:
+    #initializing the grid of neurons
     def __init__(self, height, width, input_dim):
+        #height and weight are represented by "r" in the article for inputs, and "c" for outputs
         self.height = height
         self.width = width
         self.input_dim = input_dim
+        #random variable synaptic weights associated with each neuron
         self.weights = np.random.random((height, width, input_dim))
 
     def train(self, inputs, num_epochs=100, initial_learning_rate=0.1, initial_radius=None):
+        #NOTE: article uses initial_learning_rate = 0.01, but 0.1 produces better results for us for now
         if initial_radius is None:
             initial_radius = max(self.height, self.width) / 2
         time_constant = num_epochs / np.log(initial_radius)
-        
+
+        #adjusting learning rate and weights
         for epoch in range(num_epochs):
             learning_rate = initial_learning_rate * np.exp(-epoch / num_epochs)
             radius = initial_radius * np.exp(-epoch / time_constant)
@@ -21,13 +26,28 @@ class SOM:
             for input_vec in inputs:
                 bmu_idx = self.find_bmu(input_vec)
                 for i in range(self.height):
+                    sumofweights = 0
+                    sumofweights = sumofweights + self.weights[i,:,:]
                     for j in range(self.width):
-                        dist_to_bmu = np.linalg.norm(np.array([i, j]) - np.array(bmu_idx))
+                        dist_to_bmu = np.linalg.norm(np.array([i,j]) - np.array(bmu_idx))
                         if dist_to_bmu <= radius:
                             influence = np.exp(-dist_to_bmu**2 / (2 * (radius**2)))
-                            self.weights[i, j, :] += learning_rate * influence * (input_vec - self.weights[i, j, :])
+                            #implementing hebbian to update weights at each time-step
+                            self.weights[i,j,:] = self.weights[i,j,:] + learning_rate * input_vec * influence * (input_vec - self.weights[i,j,:])
+                    #implementing subtractive normalization for each cortical unit
+
+                    #NOTE: commenting this out because it works but produces all white output, which i think will be fixed once gaussian is fixed
+                    #self.weights[i,:,:] = self.weights[i,:,:] / sumofweights
+                                
     
     def find_bmu(self, input_vec):
+        #implementing linear summation where self is represented by w_cr in article and inputs is a_r in article
+
+        #NOTE: test code, as i'm not sure this is completely right integration w/bmu but it runs
+        #bmu_idx = np.argmin(np.linalg.norm(self.weights * input_vec, axis=-1))
+        #return np.unravel_index(bmu_idx, (self.height, self.width))
+
+        #NOTE: original code:
         bmu_idx = np.argmin(np.linalg.norm(self.weights - input_vec, axis=-1))
         return np.unravel_index(bmu_idx, (self.height, self.width))
     
@@ -52,12 +72,14 @@ def generate_processed_inputs(num_samples, sigma_r=1.5, h=0.5):
         eye2 = np.random.choice([0, 1], size=(16, 16))
         
         # convolve eyes with gaussian function of standard deviation sigma_r
-        eye1_convolved = gaussian_filter(eye1, sigma=sigma_r)
-        eye2_convolved = gaussian_filter(eye2, sigma=sigma_r)
+        #eye1_convolved = gaussian_filter(eye1, sigma=sigma_r)
+        #eye2_convolved = gaussian_filter(eye2, sigma=sigma_r)
         
         # adjust activity according to ha_j + (1-h)a_j'
-        adjusted_eye1 = h * eye1_convolved + (1 - h) * eye2_convolved
-        adjusted_eye2 = h * eye2_convolved + (1 - h) * eye1_convolved
+        adjusted_eye1 = h * eye1 + (1 - h) * eye2
+        adjusted_eye2 = h * eye2 + (1 - h) * eye1
+        #adjusted_eye1 = h * eye1_convolved + (1 - h) * eye2_convolved
+        #adjusted_eye2 = h * eye2_convolved + (1 - h) * eye1_convolved
         
         # flatten and concatenate the adjusted convolved eyes to form the input vector
         input_vec = np.concatenate([adjusted_eye1.flatten(), adjusted_eye2.flatten()])
