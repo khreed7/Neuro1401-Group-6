017d76d59eead83dce3647a7572e7c53f4f255b1
Catherine Liu <catherine@Catherines-MacBook-Air-2.local>
2024-04-08 09:43:18 -0400

    bays update

--- project4/bays.py
+++ project4/bays.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 M = 100  # number of neurons in the subpopulation
 stimulus_orientation = 45  # orientation of the stimulus in degrees
 neuron_preferences = np.linspace(0, 180, M)  # preferred orientations of the neurons
-omega = 5  # concentration parameter
+omega = 0.5  # width parameter
 response_noise_std = 5  # standard deviation of response noise
-gamma = 0.1  # normalization constant
+gamma = 120  # gain constant
 alpha_p = 1  # attentional gain factor for the true stimulus location
 alpha_n = np.ones(M)  # attentional gain factors for the neurons
 T_d = 1  # decoding time window in seconds
@@ -50,34 +50,43 @@ num_simulations = 1000
 error_range = np.linspace(-np.pi, np.pi, 100)  
 num_stimuli_range = [1, 2, 4, 8]
 
-# simulate decoding error
-def simulate_decoding_error(num_stimuli, num_simulations, omega, gamma, alpha_n, T_d, neuron_preferences):
-    decoding_errors = []
-    for _ in range(num_simulations):
-        selected_preferences = np.random.choice(neuron_preferences, num_stimuli, replace=False)
-        epsilon_sample = np.deg2rad(selected_preferences - stimulus_orientation)
-        result = minimize_scalar(cos_sum, args=(epsilon_sample,), bounds=(0, 2*np.pi), method='bounded')
-        decoding_error = result.x
-        decoding_errors.append(decoding_error)
-    return np.array(decoding_errors)
+def probabilistic_decoding(normalized_responses, neuron_preferences):
+    """Decodes the stimulus orientation based on a weighted average of neuron preferences."""
+    weighted_preferences = normalized_responses * neuron_preferences
+    decoded_orientation = np.sum(weighted_preferences) / np.sum(normalized_responses)
+    return decoded_orientation
 
-# plot distribution of recall errors 
-plt.figure(figsize=(12, 8))
-
-num_simulations = 1000  
-for num_stimuli in num_stimuli_range:
-    errors = simulate_decoding_error(num_stimuli, num_simulations, omega, gamma, alpha_n, T_d, neuron_preferences)
-  
-    normalized_errors = (errors + np.pi) % (2 * np.pi) - np.pi
+def simulate_decoding_errors_for_stimuli(num_stimuli_list, num_simulations, omega, gamma, alpha_n, T_d, neuron_preferences):
+    plt.figure(figsize=(12, 8))
     
-    # plot probability density
-    density, bins = np.histogram(normalized_errors, bins=50, range=(-np.pi, np.pi), density=True)
-    centers = (bins[:-1] + bins[1:]) / 2
-    plt.plot(centers, density, label='{} Stimuli'.format(num_stimuli))
+    for num_stimuli in num_stimuli_list:
+        decoding_errors = []
+        for _ in range(num_simulations):
+            stimulus_orientations = np.random.choice(neuron_preferences, num_stimuli, replace=False)
+            mean_orientation = np.mean(stimulus_orientations)
+            
+            responses = tuning_curve(mean_orientation, neuron_preferences, omega) + np.random.normal(0, response_noise_std, M)
+            normalized_responses = divisive_normalization(responses, alpha_n, gamma)
+            
+            decoded_orientation = probabilistic_decoding(normalized_responses, neuron_preferences)
+            
+            decoding_error = np.deg2rad(decoded_orientation - mean_orientation)
+            decoding_error = (decoding_error + np.pi) % (2 * np.pi) - np.pi
+            decoding_errors.append(decoding_error)
+        
+        decoding_errors = np.array(decoding_errors)
+        density, bins = np.histogram(decoding_errors, bins=50, range=(-np.pi, np.pi), density=True)
+        centers = (bins[:-1] + bins[1:]) / 2
+        plt.plot(centers, density, label='{} Stimuli'.format(8/num_stimuli))
+    
+    plt.xlabel('Error (Radians)')
+    plt.ylabel('Probability Density')
+    plt.title('Distribution of Recall Errors for Different Numbers of Orientation Stimuli')
+    plt.legend()
+    plt.xlim(-np.pi, np.pi)
+    plt.show()
 
-plt.xlabel('Error (Radians)')
-plt.ylabel('Probability Density')
-plt.title('Distribution of Recall Errors for Different Numbers of Orientation Stimuli')
-plt.legend()
-plt.xlim(-np.pi, np.pi)
-plt.show()
+num_simulations = 1000
+num_stimuli_range = [1, 2, 4, 8]
+
+simulate_decoding_errors_for_stimuli(num_stimuli_range, num_simulations, omega, gamma, alpha_n, T_d, neuron_preferences)
